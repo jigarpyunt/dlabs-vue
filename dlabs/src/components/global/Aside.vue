27f634eab4dd5b60c447de309b734478b484ec28
jigarpyunt <shaykhabdulwahab@gmail.com>
2021-03-07 15:13:30 +0530

    Setup & Start

---
+++ dlabs/src/components/global/Aside.vue
@@ -0,0 +1,30 @@
+<template>
+    <aside>
+        <div class="container-fluid">
+            <!-- Logo Container -->
+            <div class="row">
+                <div class="col-12 logo-container">
+                    <img src="@/assets/icons/logo.svg" alt="#">
+                </div>
+            </div>
+        </div>
+    </aside>
+</template>
+<script>
+export default {
+    name: "Aside"
+}
+</script>
+<style lang="scss">
+aside {
+    width: 91px;
+    height: 100%;
+    position: fixed;
+    background: #20A6ED;
+
+    .logo-container {
+        padding: 8px;
+    }
+
+}
+</style>
---
+++ dlabs/src/views/Dashboard.vue
@@ -0,0 +1,13 @@
+<template>
+   <Aside/>
+</template>
+<script>
+import Aside from "@/components/global/Aside";
+export default {
+    name : "Dashboard",
+    components: {
+        Aside
+    }
+}
+</script>
+
